40723e5a81a2e95fcc62549c0ed13291d79bd373
Luis C <lcajachahua@gmail.com>
2023-03-10 02:52:49 -0600

    Update training.py

--- model_definitions/XGBoost/model_modules/training.py
+++ model_definitions/XGBoost/model_modules/training.py
@@ -50,7 +50,8 @@ def train(context: ModelContext, **kwargs):
 
     record_training_stats(train_df,
                           features=feature_names,
-                          predictors=[target_name],
+                          targets=[target_name],
                           categorical=[target_name],
-                          importance=feature_importance,
+                          feature_importance=feature_importance,
                           context=context)
+    
